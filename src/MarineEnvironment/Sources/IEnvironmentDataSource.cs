ca52dbd885dea43d32b179967b4c43b94e74dfc1
Yonghee Cho <33943923+pepsimanpa@users.noreply.github.com>
2026-08-26 16:01:02 +0900

    Make data source interface compatible with .NET 5

--- src/MarineEnvironment/Sources/IEnvironmentDataSource.cs
+++ src/MarineEnvironment/Sources/IEnvironmentDataSource.cs
@@ -1,13 +1,15 @@
+using System;
 using MarineEnvironment.Models;
 
-namespace MarineEnvironment.Sources;
-
-internal interface IEnvironmentDataSource : IDisposable
+namespace MarineEnvironment.Sources
 {
-    string Id { get; }
-    EnvironmentType Type { get; }
-    SourceStatus Status { get; }
-    string? StatusMessage { get; }
-    EnvironmentValue? Query(EnvironmentQuery query);
-    GridResult QueryGrid(GridQuery query);
+    internal interface IEnvironmentDataSource : IDisposable
+    {
+        string Id { get; }
+        EnvironmentType Type { get; }
+        SourceStatus Status { get; }
+        string? StatusMessage { get; }
+        EnvironmentValue? Query(EnvironmentQuery query);
+        GridResult QueryGrid(GridQuery query);
+    }
 }
